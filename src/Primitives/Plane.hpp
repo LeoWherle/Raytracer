f09be10b69da31362d6e00cdd373d22bef43de55
alberic <alberic.flour-de-saegher@epitech.eu>
2024-05-03 10:52:00 +0200

    FIX: plan hit method

---
+++ src/Primitives/Plane.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** RayBan
+** File description:
+** Plane
+*/
+
+#pragma once
+
+#include "Math/Point2D.hpp"
+#include "Math/Vector3D.hpp"
+#include "Math/Rectangle3D.hpp"
+#include "IPrimitive.hpp"
+#include "Ray.hpp"
+
+class Plane : public IPrimitive{
+    public:
+
+        Vector3D _normal;
+        Point3D _origin;
+
+        Plane(Point3D origin, Vector3D normal) :_origin(origin), _normal(normal)
+        {
+        };
+
+        double hits(const Ray &ray) const override;
+};
