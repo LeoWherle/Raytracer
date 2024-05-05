2da12b596464bbb9eb545f996549653e0a9d3c92
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-06 01:36:02 +0200

    add: Multithreading support & ImageInterface for incremental image generation

--- src/Scene/IImage.hpp
+++ src/Scene/IImage.hpp
@@ -10,9 +10,16 @@ public:
     ~IImage() = default;
     virtual void resize(uint32_t width, uint32_t height) = 0;
 
+    virtual uint32_t get_width() const = 0;
+    virtual uint32_t get_height() const = 0;
+
     virtual void set_pixel(uint32_t x, uint32_t y, Color pixel_color) = 0;
     virtual Color get_pixel(uint32_t x, uint32_t y) const = 0;
+
+    virtual std::vector<uint32_t>::iterator row_begin() = 0;
+    virtual std::vector<uint32_t>::iterator row_end() = 0;
 
 protected:
 private:
 };
+;
