111d9b64546970f22b3d088072c1820621432050
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-05 16:49:51 +0200

    fix: parameters error-handling when missing output and gui

--- src/Parameters.hpp
+++ src/Parameters.hpp
@@ -25,16 +25,20 @@ public:
     {
         _loader.parse(ac, av);
         _gui = _loader.has("gui");
-        _help = _loader.has("help");
-        if (_loader.has("output")) {
-            _output_file = _loader.get("output");
+        _help = _loader.has("help") || _loader.has("h");
+        if (_loader.has("out")) {
+            _output_file = _loader.get("out");
+        }
+        if (_loader.has("o")) {
+            _output_file = _loader.get("o");
+        }
+        if (_gui && _output_file.empty()) {
+            _output_file = "output.png";
         }
         if (_loader.has(0)) {
             _scene_file = _loader.get(0);
         } else {
-            if (!_gui) {
-                throw ArgumentsLoader::MissingArgument("Scene file not found");
-            }
+            throw ArgumentsLoader::MissingArgument("Scene file not found");
         }
     }
 
