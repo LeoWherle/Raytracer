f3a381d0566512623348df9b032a0c677fdc1ac7
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-05 14:33:45 +0200

    remove: clamp from mathUtils

--- src/Math/MathsUtils.hpp
+++ src/Math/MathsUtils.hpp
@@ -18,15 +18,6 @@ static double random_double() { return rand() / (RAND_MAX + 1.0); }
 
 static double random_double(double min, double max) { return min + (max - min) * random_double(); }
 
-static double clamp(double x, double min, double max)
-{
-    if (x < min)
-        return min;
-    if (x > max)
-        return max;
-    return x;
-}
-
 inline Vector3D random_in_unit_sphere()
 {
     while (true) {
