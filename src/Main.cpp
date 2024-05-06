8587ab1c3a6400cea7f1e80d323b1f11726db954
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-07 00:26:38 +0200

    add: basic values for incremental rendering

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,9 +14,8 @@
 #include "Materials/MetalMaterial.hpp"
 #include "Primitives/Plane.hpp"
 #include "Primitives/Sphere.hpp"
-#include "Scene/World.hpp"
 #include "Scene/IncrementalImage.hpp"
-
+#include "Scene/World.hpp"
 
 auto Main::arg_parse() -> bool
 {
@@ -44,41 +43,69 @@ auto handle_events(sf::RenderWindow &window, Camera &cam) -> bool
 #pragma GCC diagnostic ignored "-Wswitch"
         switch (event.type) {
 
+        case ::sf::Event::Resized:
+            // Work in progress
+            cam.image_width = event.size.width;
+            cam.aspect_ratio = static_cast<float>(event.size.width) / event.size.height;
+            cam.update();
+            break;
         case sf::Event::Closed:
             window.close();
             break;
         case sf::Event::KeyPressed:
+            moved = true;
             switch (event.key.code) {
             case sf::Keyboard::Escape:
+                moved = false;
                 window.close();
                 break;
             case sf::Keyboard::Key::Down:
             case sf::Keyboard::Key::S:
                 cam.move(Vector3D(0, 0, movespeed));
-                moved = true;
                 break;
             case sf::Keyboard::Key::Up:
             case sf::Keyboard::Key::Z:
                 cam.move(Vector3D(0, 0, -movespeed));
-                moved = true;
                 break;
             case sf::Keyboard::Key::Right:
             case sf::Keyboard::Key::D:
                 cam.move(Vector3D(-movespeed, 0, 0));
-                moved = true;
                 break;
             case sf::Keyboard::Key::Left:
             case sf::Keyboard::Key::Q:
                 cam.move(Vector3D(movespeed, 0, 0));
-                moved = true;
                 break;
             case sf::Keyboard::Key::Space:
                 cam.move(Vector3D(0, movespeed, 0));
-                moved = true;
                 break;
             case sf::Keyboard::Key::LShift:
                 cam.move(Vector3D(0, -movespeed, 0));
-                moved = true;
+                break;
+            case sf::Keyboard::Key::Add:
+                cam.vfov -= 1;
+                cam.update();
+                break;
+            case sf::Keyboard::Key::Subtract:
+                cam.vfov += 1;
+                cam.update();
+                break;
+            case sf::Keyboard::Key::J:
+                cam.rotate(Vector3D(0, 1, 0), 0.1f);
+                break;
+            case sf::Keyboard::Key::K:
+                cam.rotate(Vector3D(1, 0, 0), 0.1f);
+                break;
+            case sf::Keyboard::Key::L:
+                cam.rotate(Vector3D(0, 0, 1), 0.1f);
+                break;
+            case sf::Keyboard::Key::U:
+                cam.rotate(Vector3D(0, 1, 0), -0.1f);
+                break;
+            case sf::Keyboard::Key::I:
+                cam.rotate(Vector3D(1, 0, 0), -0.1f);
+                break;
+            case sf::Keyboard::Key::O:
+                cam.rotate(Vector3D(0, 0, 1), -0.1f);
                 break;
             default:
                 break;
@@ -94,20 +121,21 @@ auto handle_events(sf::RenderWindow &window, Camera &cam) -> bool
 auto Main::render_real_time() -> void
 {
     _camera.update();
-    sf::RenderWindow window(
-        sf::VideoMode(_camera.image_width, _camera.image_height), "Raytracer", sf::Style::Close
-    );
+    sf::RenderWindow window(sf::VideoMode(_camera.image_width, _camera.image_height), "Raytracer");
 
     IncrementalImage image;
     // check the time for rendering a frame
     sf::Clock clock;
     _camera.samples_per_pixel = 100;
+    _camera.max_depth = 5;
     while (window.isOpen()) {
         if (handle_events(window, _camera)) {
             image.clear();
+            _camera.max_depth = 5;
         }
+        _camera.max_depth += 10;
         clock.restart();
-        _camera.render(_world, image);
+        _camera.render<false>(_world, image);
         auto stop = clock.getElapsedTime();
         std::cout << "Rendering time: " << stop.asMilliseconds() << "ms" << std::endl;
         window.clear();
@@ -133,7 +161,7 @@ auto Main::run() -> int
     _camera.aspect_ratio = 16.0f / 9.0f;
     _camera.image_width = 400;
     _camera.samples_per_pixel = 1000;
-    _camera.max_depth = 50;
+    _camera.max_depth = 20;
     _camera.background = Color(0, 0, 0);
 
     _camera.vfov = 20;
