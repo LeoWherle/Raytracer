9a64bfe1de4f035e2db2e313cfe6663160b08f69
Yanis Moyon <yanis.moyon@epitech.eu>
2024-05-08 17:15:21 +0200

    add: operator += overload to Point3D/Point3D

--- src/Math/Point3D.hpp
+++ src/Math/Point3D.hpp
@@ -6,7 +6,6 @@
 */
 
 #pragma once
-
 #include "Vector3D.hpp"
 
 class Point3D {
@@ -26,6 +25,15 @@ public:
         return Point3D(_x + vector._x, _y + vector._y, _z + vector._z);
     }
 
+    inline Point3D &operator+=(const Point3D &dot)
+    {
+        _x += dot._x;
+        _y += dot._y;
+        _z += dot._z;
+
+        return (*this);
+    }
+
     inline Point3D operator-(const Vector3D &vector) const
     {
         return Point3D(_x - vector._x, _y - vector._y, _z - vector._z);
@@ -36,3 +44,4 @@ public:
         return Vector3D(_x - point._x, _y - point._y, _z - point._z);
     }
 };
+
