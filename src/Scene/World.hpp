5110869cf5f5ace57d794998372f9b8e080ab89c
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-09 23:38:28 +0200

    add: bvh (wip)

--- src/Scene/World.hpp
+++ src/Scene/World.hpp
@@ -24,7 +24,33 @@ public:
 
     void addPrimitive(std::unique_ptr<IPrimitive> primitive);
 
-    bool hits(const Ray &r, Interval ray_d, HitRecord &rec) const override;
+    virtual bool hits(const Ray &r, Interval ray_d, HitRecord &rec) const override;
+
+    AABB boundingBox() const override
+    {
+        Point3D min(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+        Point3D max(std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min());
+        for (const auto &primitive : primitives) {
+            AABB aabb = primitive->boundingBox();
+            min._x = std::fmin(min._x, aabb.m_min._x);
+            min._y = std::fmin(min._y, aabb.m_min._y);
+            min._z = std::fmin(min._z, aabb.m_min._z);
+            max._x = std::fmax(max._x, aabb.m_max._x);
+            max._y = std::fmax(max._y, aabb.m_max._y);
+            max._z = std::fmax(max._z, aabb.m_max._z);
+        }
+        return AABB(min, max);
+    }
+
+    Point3D centroid() const override
+    {
+        Point3D centroid;
+        for (const auto &primitive : primitives) {
+            centroid += primitive->centroid();
+        }
+        centroid /= primitives.size();
+        return centroid;
+    }
 
 protected:
 private:
