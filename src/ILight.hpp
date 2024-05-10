6fe7f812b1f6a1fcd1ace7a363de03b56c1082cd
TheodoreEpitech <theodore.magna@epitech.eu>
2024-05-10 20:47:38 +0200

    add: ILight and ALight

---
+++ src/ILight.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** RayTracer
+** File description:
+** ILight
+*/
+
+#pragma once
+
+#include "Math/Point3D.hpp"
+
+class ILight {
+public:
+    ILight() = default;
+    ~ILight() = default;
+};
